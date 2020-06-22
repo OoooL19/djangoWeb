04213b92c350804955d2a63f390b340628970f3b
Owen Li <Owen@Owens-MacBook-Pro.local>
2020-06-22 15:26:03 -0400

    ok

--- polls/views.py
+++ polls/views.py
@@ -358,6 +358,8 @@ def output(request):
 					Output.objects.filter(asin = i.asin).update(price = data['price'].replace('$','').replace(',',''), asin = data['asin'], rank = None, link = data['link'], date = data['date'])
 				else:
 					Output.objects.filter(asin = i.asin).update(price = data['price'].replace('$','').replace(',',''), asin = data['asin'], rank = re.sub("[^0-9]", "", data['rank'].replace('#','').replace(',','')), link = data['link'], date = data['date'])
+			else:
+				continue
 		return render(request, 'polls/base.html', {'alldata': alldata})
 	
 	
